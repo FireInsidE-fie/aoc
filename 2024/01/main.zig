6181f96962255b2eeb99f2b49c331228e34acb48
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-09-21 11:44:27 +0200

    lay groundwork for 24/01

--- 2024/01/main.zig
+++ 2024/01/main.zig
@@ -1,5 +1,22 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
-pub fn main() void {
-	std.debug.print("Hewwo world", .{});
+fn print_slice(slice: []const u32) void {
+    std.debug.print("[!] - Printing a slice...\n", .{});
+    for (slice) |item| {
+        std.debug.print("{}\n", .{item});
+    }
+}
+
+fn sorted_distance(list1: []const u32, list2: []const u32) u32 {
+    print_slice(list1);
+    print_slice(list2);
+    return 0;
+}
+
+test "example" {
+    const list1 = [_]u32{3, 4, 2, 1, 3, 3};
+    const list2 = [_]u32{4, 3, 5, 3, 9, 4};
+
+    try expect(sorted_distance(&list1, &list2) == 11);
 }
