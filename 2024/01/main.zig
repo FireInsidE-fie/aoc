3d70375685f39914f0b759ef4d8f5ffa19c1a18e
Aisling Fontaine <aisling.fontaine@protonmail.com>
2025-09-21 19:35:39 +0200

    attempt at working towards parsing

--- 2024/01/main.zig
+++ 2024/01/main.zig
@@ -17,23 +17,22 @@ fn sum(array: []i32) i128 {
     return result;
 }
 
-fn sorted_distance(list1: []i32, list2: []i32) u128 {
-    // print_slice(list1);
-    // print_slice(list2);
+fn parse(path: []const u8) !void {
+    const allocator = std.heap.page_allocator;
 
-    // std.sort.insertion(i32, list1, {}, std.sort.asc(i32));
-    // std.sort.insertion(i32, list2, {}, std.sort.asc(i32));
+    const file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
+    const file_size = try file.getEndPos();
 
-    // print_slice(list1);
-    // print_slice(list2);
+    const buffer = try allocator.alloc(u8, file_size);
+    _ = try file.readAll(buffer);
 
-    // var sum: u32 = 0;
-    // for (list1, list2) |item1, item2| {
-    //     sum = sum + @abs(item1 - item2);
-    //     std.debug.print("[!] - Sum currently at {}\n", .{sum});
-    // }
+    std.debug.print("{s}\n", buffer);
+}
 
-    // return sum;
+fn sorted_distance(list1: []i32, list2: []i32) !u128 {
+    // const list = parse("input.txt");
+    _ = try parse("input.txt");
 
     return @abs(sum(list1) - sum(list2));
 }
@@ -41,5 +40,5 @@ fn sorted_distance(list1: []i32, list2: []i32) u128 {
 test "example" {
     var list1 = [_]i32{ 3, 4, 2, 1, 3, 3 };
     var list2 = [_]i32{ 4, 3, 5, 3, 9, 3 };
-    try expect(sorted_distance(list1[0..], list2[0..]) == 11);
+    try expect(try sorted_distance(list1[0..], list2[0..]) == 11);
 }
